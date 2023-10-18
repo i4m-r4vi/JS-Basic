6f241da0a927005dcd4b7076923e680c06fed68f
cyber_r4vi <ayswarym04@gmail.com>
2023-10-18 22:02:54 +0530

    This is My Basics

---
+++ dataTypes.js
@@ -0,0 +1,11 @@
+let name="Ravi Shankar";//String
+
+let Young=true;//Boolean
+
+let age=19;//Integer
+
+let lastName=undefined;//undefined
+
+let trophy = null;//null
+
+
---
+++ strings.js
@@ -0,0 +1,4 @@
+let name="Ravi Shankar";
+const DOB="23-04-1997";
+let str="Hello My Name is "+name+" My Date Of Birth is "+DOB;
+console.log(str);
